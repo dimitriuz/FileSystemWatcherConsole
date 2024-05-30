0aecfe902583965e1433e6e99675fd733cfa7682
Dmitrii Leshchenko <d.leshchenko@diagnocat.com>
2024-05-30 14:03:53 +0200

    improvements

--- App.cs
+++ App.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace FileSystemWatcherConsole
 {
@@ -6,23 +7,45 @@ namespace FileSystemWatcherConsole
     {
         private readonly FileProcessor _fileProcessor;
         private readonly FileWatcherMonitor _fileMonitor;
+        private readonly ILogger<App> _logger;
         private CancellationToken ct = new CancellationToken();
 
-        public App(FileProcessor fileProcessor, IHostApplicationLifetime lifetime, FileWatcherMonitor fileMonitor)
+        public App(FileProcessor fileProcessor, IHostApplicationLifetime lifetime, FileWatcherMonitor fileMonitor, ILogger<App> logger)
         {
             _fileProcessor = fileProcessor;
             _fileMonitor = fileMonitor;
+            this._logger = logger;
             lifetime.ApplicationStarted.Register(OnStarted);
         }
 
         private async void OnStarted()
         {
             await Task.Delay(500);
-            await _fileMonitor.StartAsync(ct);
-            _fileProcessor.DeleteFile("1.stl");
-            _fileProcessor.CopyFile("1.stl");
+            await ClearDestinationFolder();
+            await _fileProcessor.DeleteFile("John, Dow 12.02.1987+01112023141403.stl");
+            await _fileProcessor.DeleteFile("John, Dow 12.02.1987+01112023141403_LowerJaw.stl");
+            await _fileProcessor.DeleteFile("John, Dow 12.02.1987+01112023141403_UpperJaw.stl");
             _fileProcessor.WaitKey();
-            _fileProcessor.DeleteFile("1.stl");
+            //await _fileMonitor.StartAsync(ct);
+            //await _fileProcessor.DeleteFile("1.stl");
+            //await _fileProcessor.CopyFile("1.stl");
+            //await _fileProcessor.LockFileUntilKeyPressed("1.stl");
+            //_fileProcessor.WaitKey();
+            //await _fileProcessor.DeleteFile("1.stl");
+            await _fileProcessor.CopyFileWithLock("1.stl",  "John, Dow 12.02.1987+01112023141403.stl", TimeSpan.FromSeconds(1));
+            await _fileProcessor.DeleteFile("John, Dow 12.02.1987+01112023141403.stl");
+            _ = _fileProcessor.CopyFileWithLock("2.stl", "John, Dow 12.02.1987+01112023141403_LowerJaw.stl", TimeSpan.FromSeconds(5));
+            _ = _fileProcessor.CopyFileWithLock("3.stl", "John, Dow 12.02.1987+01112023141403_UpperJaw.stl");
+
+            _logger.LogInformation("All tasks done. Press Ctrl+C to close the app");
+        }
+
+        private async Task ClearDestinationFolder()
+        {
+            foreach(var file in Directory.EnumerateFiles("files"))
+            {
+                await _fileProcessor.DeleteFile(Path.GetFileName(file));
+            }
         }
 
         public Task StartAsync(CancellationToken cancellationToken)
@@ -30,14 +53,10 @@ namespace FileSystemWatcherConsole
             return Task.CompletedTask;
         }
 
-        public async Task StartedAsync(CancellationToken cancellationToken)
+        public Task StartedAsync(CancellationToken cancellationToken)
         {
-            return; // Task.CompletedTask;
-            await Task.Delay(500);
-            _fileProcessor.DeleteFile("1.stl");
-            _fileProcessor.CopyFile("1.stl");
-            _fileProcessor.WaitKey();
-            _fileProcessor.DeleteFile("1.stl");
+            return Task.CompletedTask;
+            
         }
 
         public Task StartingAsync(CancellationToken cancellationToken)
--- FileProcessor.cs
+++ FileProcessor.cs
@@ -1,33 +1,77 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System.IO;
 
 public class FileProcessor
 {
     IConfiguration _configuration;
     private readonly ILogger<FileProcessor> _logger;
-    private string destination;
-    private string GetSourcePath(string source) => @$"files\{source}";
+    private string destinationPath;
+    private string GetSourcePath(string filename) => @$"files\{filename}";
+    private string GetDestinationPath(string filename) => Path.Combine(destinationPath, filename);
     public FileProcessor(IConfiguration configuration, ILogger<FileProcessor> logger)
     {
         _configuration = configuration;
         _logger = logger;
-        destination = _configuration.GetSection("path").Value;
+        destinationPath = _configuration.GetSection("path").Value;
     }
 
-    public void CopyFile(string source)
+    public async Task CopyFile(string filename, string? destinationFilename = null)
     {
-        _logger.LogInformation("Copying file {source}", source);
-        var filename = Path.GetFileName(source);
-        File.Copy(GetSourcePath(source), Path.Combine(destination, filename));
+        _logger.LogInformation("Copying file {source}", filename);
+
+        var source = GetSourcePath(filename);
+        var destination = GetDestinationPath(destinationFilename is null ? filename : destinationFilename);
+        File.Copy(source, destination);
     }
 
-    public void DeleteFile(string filename)
+    public async Task CopyFileWithLock(string filename, string? destinationFilename = null, TimeSpan delay = default)
     {
-        var source = Path.Combine(destination, filename);
-        if (File.Exists(source))
+        var source = GetSourcePath(filename);
+        var destination = GetDestinationPath(destinationFilename is null ? filename : destinationFilename);
+        _logger.LogInformation("Copying file with lock {source}", filename);
+
+        using var sourceStream = new FileStream(source, FileMode.Open, FileAccess.Read, FileShare.None);
+        using var destinationStream = new FileStream(destination, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None);
+        await sourceStream.CopyToAsync(destinationStream);
+        await Task.Delay(delay);
+        _logger.LogInformation("File unlocked {source}", filename);
+    }
+
+    public Task DeleteFile(string filename)
+    {
+        var file = GetDestinationPath(filename);
+        if (File.Exists(file))
         {
             _logger.LogInformation("Deleting file {filename}", filename);
-            File.Delete(source);
+            File.Delete(file);
+        }
+        return Task.CompletedTask;
+    }
+
+    public async Task LockFileUntilKeyPressed(string filename)
+    {
+        var file = GetDestinationPath(filename);
+        var _lock = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.None);
+        _logger.LogInformation("File locked {filename}", filename);
+        WaitKey();
+        if (_lock != null)
+        {
+            _lock.Dispose();
+            _logger.LogInformation("File unlocked {filename}", filename);
+        }
+    }
+
+    public async Task LockFile(string filename, TimeSpan time)
+    {
+        var file = GetDestinationPath(filename);
+        var _lock = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.None);
+        _logger.LogInformation("File locked {filename}", filename);
+        await Task.Delay(time);
+        if (_lock != null)
+        {
+            _lock.Dispose();
+            _logger.LogInformation("File unlocked {filename}", filename);
         }
     }
 
