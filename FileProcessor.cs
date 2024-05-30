9c8dfc2535385559c66f14806cfd5e78ef5f0c79
Dmitrii Leshchenko <d.leshchenko@diagnocat.com>
2024-05-30 11:24:15 +0200

    Add project files.

---
+++ FileProcessor.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+public class FileProcessor
+{
+    IConfiguration _configuration;
+    private readonly ILogger<FileProcessor> _logger;
+    private string destination;
+    private string GetSourcePath(string source) => @$"files\{source}";
+    public FileProcessor(IConfiguration configuration, ILogger<FileProcessor> logger)
+    {
+        _configuration = configuration;
+        _logger = logger;
+        destination = _configuration.GetSection("path").Value;
+    }
+
+    public void CopyFile(string source)
+    {
+        _logger.LogInformation("Copying file {source}", source);
+        var filename = Path.GetFileName(source);
+        File.Copy(GetSourcePath(source), Path.Combine(destination, filename));
+    }
+
+    public void DeleteFile(string filename)
+    {
+        var source = Path.Combine(destination, filename);
+        if (File.Exists(source))
+        {
+            _logger.LogInformation("Deleting file {filename}", filename);
+            File.Delete(source);
+        }
+    }
+
+    public void WaitKey()
+    {
+        Console.WriteLine("Press any key to continue");
+        Console.ReadKey();
+    }
+}
---
+++ FileWatcherMonitor.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.IO;
+
+public class FileWatcherMonitor
+{
+    private readonly IConfiguration _configuration;
+    private readonly ILogger<FileWatcherMonitor> _logger;
+    private FileSystemWatcher _fileSystemWatcher;
+
+    public FileWatcherMonitor(IConfiguration configuration, ILogger<FileWatcherMonitor> logger)
+    {
+        _configuration = configuration;
+        _logger = logger;
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        return StartFileWatcher(_configuration.GetSection("path").Value);
+    }
+
+    public async Task StartFileWatcher(string path)
+    {
+        _fileSystemWatcher = new FileSystemWatcher
+        {
+            Path = path,
+
+            NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName,
+
+            IncludeSubdirectories = true,
+
+            InternalBufferSize = 64 * 1024
+        };
+
+        _fileSystemWatcher.Created += ProcessFileSystemWatcherEvent;
+        _fileSystemWatcher.Changed += ProcessFileSystemWatcherEvent;
+        _fileSystemWatcher.Renamed += ProcessFileSystemWatcherEvent;
+        _fileSystemWatcher.Deleted += ProcessFileSystemWatcherEvent;
+
+        _logger.LogInformation("Starting to watch folder {path}", path);
+        _fileSystemWatcher.EnableRaisingEvents = true;
+    }
+
+    void ProcessFileSystemWatcherEvent(object e, FileSystemEventArgs args)
+    {
+        _logger.LogInformation("{ChangeType} event fired for file {path}", args.ChangeType, args.FullPath);
+    }
+}
