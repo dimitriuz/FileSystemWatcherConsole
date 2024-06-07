5eb38ca220f55b9e3cee10c81f1c2d4eb0a0ad1f
Dmitrii Leshchenko <d.leshchenko@diagnocat.com>
2024-06-07 16:26:49 +0200

    folder ops

--- App.cs
+++ App.cs
@@ -17,9 +17,9 @@ namespace FileSystemWatcherConsole
             _logger = logger;
         }
 
-        private async Task ClearDestinationFolder()
+        private async Task ClearFolder(string path)
         {
-            foreach (var file in Directory.EnumerateFiles("files"))
+            foreach (var file in Directory.EnumerateFiles(path))
             {
                 await _fileProcessor.DeleteFile(Path.GetFileName(file));
             }
@@ -44,30 +44,45 @@ namespace FileSystemWatcherConsole
         public async Task StartedAsync(CancellationToken cancellationToken)
         {
             await _fileMonitor.StartAsync(_ct.Token);
-            //await ProcessFilesWithProcessor();
+            await ProcessFilesWithProcessor();
         }
 
         private async Task ProcessFilesWithProcessor()
         {
-            await ClearDestinationFolder();
-            await _fileProcessor.DeleteFile("John, Dow 12.02.1987+01112023141403.stl");
-            await _fileProcessor.DeleteFile("John, Dow 12.02.1987+01112023141403_LowerJaw.stl");
-            await _fileProcessor.DeleteFile("John, Dow 12.02.1987+01112023141403_UpperJaw.stl");
+            // arrange
+            await _fileProcessor.DeleteFolder("d:\\features\\BNB-499\\case\\2e6da928-99da-49ab-acaf-2ec0b8db5171\\TASK_f52a35d1-a757-4357-868c-20f0cbba0987\\VOL_MAR");
             _fileProcessor.WaitKey();
+
+            //await _fileProcessor.DeleteFile("John, Dow 12.02.1987+01112023141403.stl");
+            //await _fileProcessor.DeleteFile("John, Dow 12.02.1987+01112023141403_LowerJaw.stl");
+            //await _fileProcessor.DeleteFile("John, Dow 12.02.1987+01112023141403_UpperJaw.stl");
+
+            /*
+            ProcessFileEvent "I:\TW\5406ebc1-c9fc-4f11-a70e-776c01bd035f\.csi_data\.version_4.4\.tmp\8c2c98bc\NSP_NSP_20200111_022918\3DSlice500.dcm"
+            ProcessFileEvent "I:\TW\5406ebc1-c9fc-4f11-a70e-776c01bd035f\.csi_data\.version_4.4\.tmp\8c2c98bc\NSP_NSP_20200111_022918\VOL_MAR\3DSlice500.dcm"
+             */
+
+            // act
+            await _fileProcessor.CopyFolder("d:\\features\\BNB-499\\preparing\\VOL_MAR"
+                , "d:\\features\\BNB-499\\case\\2e6da928-99da-49ab-acaf-2ec0b8db5171\\.csi_data\\.version_4.4\\.tmp\\VOL_MAR");
+
+            await _fileProcessor.MoveFolder("d:\\features\\BNB-499\\case\\2e6da928-99da-49ab-acaf-2ec0b8db5171\\.csi_data\\.version_4.4\\.tmp\\VOL_MAR"
+                , "d:\\features\\BNB-499\\case\\2e6da928-99da-49ab-acaf-2ec0b8db5171\\TASK_f52a35d1-a757-4357-868c-20f0cbba0987\\VOL_MAR");
+            _fileProcessor.WaitKey();
+
 
             //await _fileProcessor.DeleteFile("1.stl");
             //await _fileProcessor.CopyFile("1.stl");
             //await _fileProcessor.LockFileUntilKeyPressed("1.stl");
-            //_fileProcessor.WaitKey();
             //await _fileProcessor.DeleteFile("1.stl");
-            await _fileProcessor.CopyFileWithLock("1.stl", "John, Dow 12.02.1987+01112023141403.stl", TimeSpan.FromSeconds(1));
-            await _fileProcessor.DeleteFile("John, Dow 12.02.1987+01112023141403.stl");
+            //await _fileProcessor.CopyFileWithLock("1.stl", "John, Dow 12.02.1987+01112023141403.stl", TimeSpan.FromSeconds(1));
+            //await _fileProcessor.DeleteFile("John, Dow 12.02.1987+01112023141403.stl");
 
-            await Task.WhenAll
-                (
-                _fileProcessor.CopyFileWithLock("2.stl", "John, Dow 12.02.1987+01112023141403_LowerJaw.stl", TimeSpan.FromSeconds(10)),
-                _fileProcessor.CopyFileWithLock("3.stl", "John, Dow 12.02.1987+01112023141403_UpperJaw.stl")
-                );
+            //await Task.WhenAll
+            //    (
+            //    _fileProcessor.CopyFileWithLock("2.stl", "John, Dow 12.02.1987+01112023141403_LowerJaw.stl", TimeSpan.FromSeconds(10)),
+            //    _fileProcessor.CopyFileWithLock("3.stl", "John, Dow 12.02.1987+01112023141403_UpperJaw.stl")
+            //    );
 
             //_logger.LogInformation("All tasks done. Press Ctrl+C to close the app");
         }
--- FileProcessor.cs
+++ FileProcessor.cs
@@ -7,13 +7,27 @@ public class FileProcessor
     IConfiguration _configuration;
     private readonly ILogger<FileProcessor> _logger;
     private string destinationPath;
-    private string GetSourcePath(string filename) => @$"files\{filename}";
+    private string GetSourcePath(string filename) 
+    {
+        string source;
+
+        if (!Path.IsPathFullyQualified(filename))
+        {
+            source = @$"files\{filename}";
+        }
+        else
+        {
+                source = filename;
+        };
+
+        return source;
+    }
     private string GetDestinationPath(string filename) => Path.Combine(destinationPath, filename);
     public FileProcessor(IConfiguration configuration, ILogger<FileProcessor> logger)
     {
         _configuration = configuration;
         _logger = logger;
-        destinationPath = _configuration.GetSection("path").Value;
+        destinationPath = Environment.GetCommandLineArgs()[1];
     }
 
     public async Task CopyFile(string filename, string? destinationFilename = null)
@@ -23,6 +37,51 @@ public class FileProcessor
         var source = GetSourcePath(filename);
         var destination = GetDestinationPath(destinationFilename is null ? filename : destinationFilename);
         File.Copy(source, destination);
+    }
+
+    public async Task CopyFolder(string sourceDir, string? destinationFilename = null, bool recursive = true)
+    {
+        var source = GetSourcePath(sourceDir);
+
+        _logger.LogInformation("Copying folder {source}", source);
+
+        var destination = GetDestinationPath(destinationFilename is null ? sourceDir : destinationFilename);
+        
+        var dir = new DirectoryInfo(source);
+
+        // Cache directories before we start copying
+        DirectoryInfo[] dirs = dir.GetDirectories();
+
+        // Create the destination directory
+        Directory.CreateDirectory(destination);
+
+        // Get the files in the source directory and copy to the destination directory
+        foreach (FileInfo file in dir.GetFiles())
+        {
+            string targetFilePath = Path.Combine(destination, file.Name);
+            file.CopyTo(targetFilePath);
+        }
+
+        // If recursive and copying subdirectories, recursively call this method
+        if (recursive)
+        {
+            foreach (DirectoryInfo subDir in dirs)
+            {
+                string newDestinationDir = Path.Combine(destination, subDir.Name);
+                CopyFolder(subDir.FullName, newDestinationDir, true);
+            }
+        }
+    }
+
+    public async Task MoveFolder(string sourceDir, string? destinationFilename = null)
+    {
+        var source = GetSourcePath(sourceDir);
+
+        _logger.LogInformation("Moving folder {source}", source);
+
+        var destination = GetDestinationPath(destinationFilename is null ? sourceDir : destinationFilename);
+
+        Directory.Move(source, destination);
     }
 
     public async Task CopyFileWithLock(string filename, string? destinationFilename = null, TimeSpan delay = default)
@@ -43,22 +102,35 @@ public class FileProcessor
         var file = GetDestinationPath(filename);
         if (File.Exists(file))
         {
-            _logger.LogInformation("Deleting file {filename}", filename);
+            _logger.LogInformation("Deleting file {sourceDir}", filename);
             File.Delete(file);
         }
         return Task.CompletedTask;
     }
 
+    public Task DeleteFolder(string path)
+    {
+        var source = GetSourcePath(path);
+        if (Directory.Exists(source))
+        {
+            _logger.LogInformation("Deleting directory {sourceDir}", source);
+            Directory.Delete(source, true);
+        }
+
+        return Task.CompletedTask;
+    }
+
+
     public async Task LockFileUntilKeyPressed(string filename)
     {
         var file = GetDestinationPath(filename);
         var _lock = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.None);
-        _logger.LogInformation("File locked {filename}", filename);
+        _logger.LogInformation("File locked {sourceDir}", filename);
         WaitKey();
         if (_lock != null)
         {
             _lock.Dispose();
-            _logger.LogInformation("File unlocked {filename}", filename);
+            _logger.LogInformation("File unlocked {sourceDir}", filename);
         }
     }
 
@@ -66,12 +138,12 @@ public class FileProcessor
     {
         var file = GetDestinationPath(filename);
         var _lock = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.None);
-        _logger.LogInformation("File locked {filename}", filename);
+        _logger.LogInformation("File locked {sourceDir}", filename);
         await Task.Delay(time);
         if (_lock != null)
         {
             _lock.Dispose();
-            _logger.LogInformation("File unlocked {filename}", filename);
+            _logger.LogInformation("File unlocked {sourceDir}", filename);
         }
     }
 
